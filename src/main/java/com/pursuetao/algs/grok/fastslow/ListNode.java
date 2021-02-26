0bb4eeb517c9334df9227cae28a1e2599044bedb
Faye Lee <wizard@pursuetao.com>
2021-02-26 15:08:09 +0800

    feat: ListNode class

---
+++ src/main/java/com/pursuetao/algs/grok/fastslow/ListNode.java
@@ -0,0 +1,15 @@
+package com.pursuetao.algs.grok.fastslow;
+
+public class ListNode {
+    public int value;
+    public ListNode next = null;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}

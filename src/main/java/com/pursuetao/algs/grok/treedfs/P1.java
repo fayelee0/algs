28105d69813ac12361714f15fabe447419a96f75
Faye Lee <wizard@pursuetao.com>
2021-03-05 00:15:52 +0800

    fix: todo for practice

---
+++ src/main/java/com/pursuetao/algs/grok/treedfs/P1.java
@@ -0,0 +1,11 @@
+package com.pursuetao.algs.grok.treedfs;
+
+// Tree Diameter
+//
+// Given a binary tree, find the length of its diameter.
+// The diameter of a tree is the number of nodes on the longest path between any two leaf nodes.
+// The diameter of a tree may or may not pass through the root.
+// Note: You can always assume that there are at least two leaf nodes in the given tree.
+public class P1 {
+    // TODO
+}
---
+++ src/main/java/com/pursuetao/algs/grok/treedfs/P2.java
@@ -0,0 +1,11 @@
+package com.pursuetao.algs.grok.treedfs;
+
+// Path with Maximum Sum
+//
+// Find the path with the maximum sum in a given binary tree.
+// Write a function that returns the maximum sum.
+// A path can be defined as a sequence of nodes between any two nodes and doesn't necessarily pass through the root.
+// The path must contain at least one node.
+public class P2 {
+    // TODO
+}
